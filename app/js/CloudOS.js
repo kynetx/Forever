866b5fa9288955cf6f63b32ee0fb97ad51ae5141
Ed Orcutt <edo@lobosllc.com>
2013-04-26 16:51:37 -0600

    Build URL that will start the OAuth dance with SquareTag #6

--- app/js/CloudOS.js
+++ app/js/CloudOS.js
@@ -4,6 +4,10 @@
 	// Personal Cloud Hostname
 	var CloudOS_Host = "kibdev.kobj.net";
 		window.CloudOS_Host = CloudOS_Host;
+	var CloudOS_App_Key = "67591932-AD30-11E2-AF99-7B86E71C24E1";
+		window.CloudOS_App_Key = CloudOS_App_Key;
+	var CloudOS_Callback_URL = "http://devcloud.krlcode.com/";
+		window.CloudOS_Callback_URL = CloudOS_Callback_URL;
 	var CloudOS_Session_Token = "none";
 		window.CloudOS_Session_Token = CloudOS_Session_Token;
 
@@ -36,5 +40,18 @@
 		);
 	}
 	window.CloudOS_Destroy_Channel = CloudOS_Destroy_Channel;
+
+	// ------------------------------------------------------------------------
+	function CloudOS_Get_OAuth_URL() {
+		var client_state = Math.floor(Math.random()*9999999);
+		var url = 'https://' + CloudOS_Host +
+			'/oauth/authorize?response_type=code' +
+			'&redirect_uri=' + encodeURIComponent(CloudOS_Callback_URL) +
+			'&client_id=' + CloudOS_App_Key +
+			'&state=' + client_state;
+
+		return(url)
+	}
+	window.CloudOS_Get_OAuth_URL = CloudOS_Get_OAuth_URL;
 
 })();
